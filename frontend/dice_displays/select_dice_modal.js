223b8cc25de4ca5314a2589429ef6f1412198fbc
alex abrams <alexander.abrams99@gmail.com>
2022-08-17 15:16:57 -0700

    Added additional styling to the modal pressables.

--- frontend/dice_displays/select_dice_modal.js
+++ frontend/dice_displays/select_dice_modal.js
@@ -10,35 +10,65 @@ class SelectDiceModal extends React.Component {
 
   render() {
     return(
-      <View style={{height: 250, width: 250, borderRadius: 20,
-           position: 'absolute', alignSelf: 'center', marginTop: 200, zIndex: 5, backgroundColor: 'white',
-           shadowColor: "#000",
-           shadowOffset: { width: 0, height: 2 },
-           shadowOpacity: 0.25,
-           shadowRadius: 4,
-           elevation: 5
-         }}>
-         <Pressable onPress={() => this.props.modal_actions.receiveDiceSelection('test')}>
-           <Text>Thing </Text>
+      <View style={styles.modal_container}>
+
+        <View style={styles.modal_content}>
+         <Pressable style={styles.pressables} onPress={() => this.props.modal_actions.receiveDiceSelection('dice-6')}>
+           <Text style={styles.text}>6 - Sided</Text>
          </Pressable>
-         <Pressable onPress={() => console.log('presddddddsed!')}>
-           <Text>Thing </Text>
+
+         <Pressable style={styles.pressables} onPress={() => this.props.modal_actions.receiveDiceSelection('dice-4')}>
+           <Text style={styles.text}>4 - Sided</Text>
          </Pressable>
-         <Pressable onPress={() => console.log('pre54534543ssed!')}>
-           <Text>Thing </Text>
+
+         <Pressable style={styles.pressables} onPress={() => this.props.modal_actions.receiveDiceSelection('dice-8')}>
+           <Text style={styles.text}>8 - Sided</Text>
          </Pressable>
-         <Pressable onPress={() => console.log('pres;;;;;sed!')}>
-           <Text>Thing </Text>
+
+         <Pressable style={styles.pressables} onPress={() => this.props.modal_actions.receiveDiceSelection('dice-10')}>
+           <Text style={styles.text}>10 - Sided</Text>
          </Pressable>
-         <Pressable onPress={() => console.log('pres/////sed!')}>
-           <Text>Thing </Text>
+
+         <Pressable style={styles.pressables} onPress={() => this.props.modal_actions.receiveDiceSelection('dice-12')}>
+           <Text style={styles.text}>12 - Sided</Text>
          </Pressable>
-         <Pressable onPress={() => console.log('pressed!')}>
-           <Text>Thing </Text>
+
+         <Pressable style={styles.pressables} onPress={() => this.props.modal_actions.receiveDiceSelection('dice-20')}>
+           <Text style={styles.text}>20 - Sided</Text>
          </Pressable>
+       </View>
+
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  modal_container: {
+    height: 250,
+    width: 250,
+    borderRadius: 20,
+    position: 'absolute',
+    alignSelf: 'center',
+    marginTop: 180,
+    zIndex: 5,
+    backgroundColor: '#f2f2f2',
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  modal_content: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  pressables: {
+    marginTop: 18,
+  },
+  text: {
+    color:'#4db8ff',
+  }
+})
 
 export default SelectDiceModal;
